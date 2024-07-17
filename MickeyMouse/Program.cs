576550d2a0d8ad6d519f3d68362d35226f307341
Thunderbolt1029 <105086190+Thunderbolt1029@users.noreply.github.com>
2024-07-17 16:25:23 +0100

    Removed test code

--- MickeyMouse/Program.cs
+++ MickeyMouse/Program.cs
@@ -100,28 +100,6 @@ namespace MickeyMouse
                 };
             }
 
-            // TEST CODE
-            Scored[0] = new Dictionary<string, int>()
-                {
-                    { "20", 0 },
-                    { "19", 0 },
-                    { "18", 0 },
-                    { "17", 0 },
-                    { "16", 0 },
-                    { "15", 0 },
-                    { "14", 0 },
-                    { "13", 0 },
-                    { "12", 0 },
-                    { "11", 0 },
-                    { "10", 0 },
-                    { "D", NoNeededToScore },
-                    { "T", 0 },
-                    { "25", 0 },
-                };
-
-            Scores[1].Add(2);
-
-            // TEST CODE ENDS
 
             while (!GameWon)
             {
